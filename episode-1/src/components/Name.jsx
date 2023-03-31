2507b8d3be2438f1c2a8c9110bc0f5b4af6e4e72
ISHIMWE Isaac <ixacson@gmail.com>
2023-03-31 14:39:36 +0200

    Deleting already exist files

--- episode-1/src/components/Name.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Name({ name }) {
-  return <h1>Hello {name}</h1>;
-}
--- react-todo-list/src/App.jsx
+++ react-todo-list/src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // All tasks created will be stored in this array
+  const [todolist, setTodoList] = useState([]);
+  // New task typed will be stored in this variable
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (e) => {
+    setNewTask(e.target.value);
+  };
+
+  const addTask = () => {
+    // Add new task to the todolist array
+    setTodoList([...todolist, newTask]);
+    // Clear the input field
+    setNewTask("");
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>To Do List</h1>
+      <div className="add-task">
+        <input
+          type="text"
+          placeholder="Type your task here"
+          onChange={handleChange}
+        />
+        <button onClick={addTask}>Add task</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="todos-container">
+        {todolist.map((task, key) => {
+          return (
+            <p key={key} className="task">
+              {task}
+            </p>
+          );
+        })}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
